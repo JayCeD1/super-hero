17600f003d1392534fdc4b0fbc4a80318e39e60c
JayCeD1 <jesseeren@gmail.com>
2024-05-07 16:28:24 +0300

    mocking rest clients

--- quarkus-super-heroes/super-heroes/rest-fights/src/test/java/erenes/fight/client/MockVillainProxy.java
+++ quarkus-super-heroes/super-heroes/rest-fights/src/test/java/erenes/fight/client/MockVillainProxy.java
@@ -1,4 +1,17 @@
 package erenes.fight.client;
 
-public class MockVillainProxy {
+import erenes.org.fight.client.Villain;
+import erenes.org.fight.client.VillainProxy;
+import io.quarkus.test.Mock;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Mock
+@ApplicationScoped
+@RestClient
+public class MockVillainProxy implements VillainProxy {
+    @Override
+    public Villain findRandomVillain() {
+        return DefaultTestVillain.INSTANCE;
+    }
 }
