66ffbbfdd421e000a11ca9b27de268a4bcce81e6
JayCeD1 <jesseeren@gmail.com>
2024-05-07 14:54:09 +0300

    hero service added and resource

---
+++ quarkus-super-heroes/super-heroes/rest-heroes/src/test/java/erenes/org/hero/HeroResourceIT.java
@@ -0,0 +1,8 @@
+package erenes.org.hero;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HeroResourceIT extends HeroResourceTest {
+    // Execute the same tests but in packaged mode.
+}
