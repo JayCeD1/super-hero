f7e00ceaa0cb43aa429bb07adbe06058ea61afdd
JayCeD1 <jesseeren@gmail.com>
2024-05-07 10:58:31 +0300

    Initial commit microserving in quarkus

---
+++ quarkus-super-heroes/super-heroes/rest-villains/src/test/java/erenes/org/VillainResourceIT.java
@@ -0,0 +1,8 @@
+package erenes.org;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class VillainResourceIT extends VillainResourceTest {
+    // Execute the same tests but in packaged mode.
+}
