57c0766b56438c5463846b43d24a45836dc63c68
JayCeD1 <jesseeren@gmail.com>
2024-05-07 16:25:14 +0300

    moved

--- quarkus-super-heroes/super-heroes/rest-fights/src/main/java/erenes/org/fight/Fighters.java
+++ quarkus-super-heroes/super-heroes/rest-fights/src/main/java/erenes/org/fight/Fighters.java
@@ -1,7 +1,7 @@
 package erenes.org.fight;
 
-import erenes.org.client.Hero;
-import erenes.org.client.Villain;
+import erenes.org.fight.client.Hero;
+import erenes.org.fight.client.Villain;
 import jakarta.validation.constraints.NotNull;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
